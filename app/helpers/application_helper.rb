90562b0bd661cb91f6aabd12efa5b659ae03804d
o-t-k-t <wetcondition@gmail.com>
2019-01-09 14:07:19 +0900

    ウィザード説明の追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,9 @@
 module ApplicationHelper
-  def dollcost_average_reference_path
-    'https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%AB%E3%83%BB%E3%82%B3%E3%82%B9%E3%83%88%E5%B9%B3%E5%9D%87%E6%B3%95'
-  end
-
-  def trailing_stop_reference_path
-    'http://www.theacademy-ibt.com/'
-  end
-
   def coincheck_reference_path
     'https://coincheck.com/ja/'
+  end
+
+  def btc_jpy_market_path
+    'https://coincheck.com/ja/exchange/charts/'
   end
 end
