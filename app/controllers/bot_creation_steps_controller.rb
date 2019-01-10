761ca9f5ab2a2610435b2251b89e7cce4e19957e
o-t-k-t <wetcondition@gmail.com>
2019-01-10 13:39:17 +0900

    ウィザード入力不正時の指摘

--- app/controllers/bot_creation_steps_controller.rb
+++ app/controllers/bot_creation_steps_controller.rb
@@ -8,16 +8,21 @@ class BotCreationStepsController < ApplicationController
     when :type_selection
       @bot = Bot.new.decorate
       @currency_pairs = CurrencyPair.all
+    when :parameter_input
+      @bot = Bot.make(takeover_bot_params)&.decorate
+      @bot.user = current_user
+      @currency_pairs = CurrencyPair.all
+      @bot.valid?
     else
       redirect_to bots_path and return
     end
 
-    render_wizard @bot
+    render_wizard
   end
 
   def update # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
     @bot = Bot.make(bot_params)&.decorate
-    @bot.user = current_user
+    @currency_pairs = CurrencyPair.all
 
     case step
     when :parameter_input
@@ -26,11 +31,13 @@ class BotCreationStepsController < ApplicationController
         redirect_to wizard_path(:type_selection) and return
       end
     when :confirmation
-      unless @bot.valid?
-        flash[:notice] = I18n.t('bots.confirmation_fail')
-        redirect_to wizard_path(:type_selection) and return
+      @bot.user = current_user
+      if @bot.invalid?
+        jump_to(:parameter_input, takeover_params: bot_params)
+        # render :parameter_input and return
       end
     when :creation
+      @bot.user = current_user
       @bot.save!
       flash[:notice] = I18n.t('bots.creation_success')
       redirect_to bots_path and return
@@ -54,6 +61,20 @@ class BotCreationStepsController < ApplicationController
       :ts_key_amount
     )
   end
+
+  def takeover_bot_params
+    params.require(:takeover_params)&.permit(
+      :type,
+      :currency_pair_id,
+      :level_base,
+      :level_slope,
+      :dca_settlment_amount,
+      :dca_interval_unit,
+      :dca_interval_value,
+      :ts_key_amount
+    )
+  end
+
 
   def finish_wizard_path
     bots_path
